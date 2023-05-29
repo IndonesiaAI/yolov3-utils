18a91eccf64afb4e963942caa0ca9bba5699b81d
rubythalib33 <rubyabdullah14@gmail.com>
2023-05-29 08:14:47 +0700

    update

---
+++ utils.py
@@ -0,0 +1,219 @@
+import torch
+import numpy as np
+import torchvision
+import time
+
+def xyxy2xywh(x):
+    # Convert nx4 boxes from [x1, y1, x2, y2] to [x, y, w, h] where xy1=top-left, xy2=bottom-right
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = (x[:, 0] + x[:, 2]) / 2  # x center
+    y[:, 1] = (x[:, 1] + x[:, 3]) / 2  # y center
+    y[:, 2] = x[:, 2] - x[:, 0]  # width
+    y[:, 3] = x[:, 3] - x[:, 1]  # height
+    return y
+
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
+
+def box_iou(box1, box2):
+    # https://github.com/pytorch/vision/blob/master/torchvision/ops/boxes.py
+    """
+    Return intersection-over-union (Jaccard index) of boxes.
+    Both sets of boxes are expected to be in (x1, y1, x2, y2) format.
+    Arguments:
+        box1 (Tensor[N, 4])
+        box2 (Tensor[M, 4])
+    Returns:
+        iou (Tensor[N, M]): the NxM matrix containing the pairwise
+            IoU values for every element in boxes1 and boxes2
+    """
+
+    def box_area(box):
+        # box = 4xn
+        return (box[2] - box[0]) * (box[3] - box[1])
+
+    area1 = box_area(box1.T)
+    area2 = box_area(box2.T)
+
+    # inter(N,M) = (rb(N,M,2) - lt(N,M,2)).clamp(0).prod(2)
+    inter = (torch.min(box1[:, None, 2:], box2[:, 2:]) - torch.max(box1[:, None, :2], box2[:, :2])).clamp(0).prod(2)
+    return inter / (area1[:, None] + area2 - inter) 
+  
+def scale_to_coords(prediction):
+    #postprocess
+    pass
+
+def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=None, agnostic=False, multi_label=False,
+                        labels=()):
+    """Runs Non-Maximum Suppression (NMS) on inference results
+
+    Returns:
+         list of detections, on (n,6) tensor per image [xyxy, conf, cls]
+    """
+
+    nc = prediction.shape[2] - 5  # number of classes
+    xc = prediction[..., 4] > conf_thres  # candidates
+
+    # Settings
+    min_wh, max_wh = 2, 4096  # (pixels) minimum and maximum box width and height
+    max_det = 300  # maximum number of detections per image
+    max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
+    time_limit = 10.0  # seconds to quit after
+    redundant = True  # require redundant detections
+    multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
+    merge = False  # use merge-NMS
+
+    t = time.time()
+    output = [torch.zeros((0, 6), device=prediction.device)] * prediction.shape[0]
+    for xi, x in enumerate(prediction):  # image index, image inference
+        # Apply constraints
+        # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
+        x = x[xc[xi]]  # confidence
+
+        # Cat apriori labels if autolabelling
+        if labels and len(labels[xi]):
+            l = labels[xi]
+            v = torch.zeros((len(l), nc + 5), device=x.device)
+            v[:, :4] = l[:, 1:5]  # box
+            v[:, 4] = 1.0  # conf
+            v[range(len(l)), l[:, 0].long() + 5] = 1.0  # cls
+            x = torch.cat((x, v), 0)
+
+        # If none remain process next image
+        if not x.shape[0]:
+            continue
+
+        # Compute conf
+        if nc == 1:
+            x[:, 5:] = x[:, 4:5] # for models with one class, cls_loss is 0 and cls_conf is always 0.5,
+                                 # so there is no need to multiplicate.
+        else:
+            x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
+
+        # Box (center x, center y, width, height) to (x1, y1, x2, y2)
+        box = xywh2xyxy(x[:, :4])
+
+        # Detections matrix nx6 (xyxy, conf, cls)
+        if multi_label:
+            i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
+            x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
+        else:  # best class only
+            conf, j = x[:, 5:].max(1, keepdim=True)
+            x = torch.cat((box, conf, j.float()), 1)[conf.view(-1) > conf_thres]
+
+        # Filter by class
+        if classes is not None:
+            x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
+
+        # Apply finite constraint
+        # if not torch.isfinite(x).all():
+        #     x = x[torch.isfinite(x).all(1)]
+
+        # Check shape
+        n = x.shape[0]  # number of boxes
+        if not n:  # no boxes
+            continue
+        elif n > max_nms:  # excess boxes
+            x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence
+
+        # Batched NMS
+        c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
+        boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
+        i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
+        if i.shape[0] > max_det:  # limit detections
+            i = i[:max_det]
+        if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
+            # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
+            iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
+            weights = iou * scores[None]  # box weights
+            x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
+            if redundant:
+                i = i[iou.sum(1) > 1]  # require redundancy
+
+        output[xi] = x[i]
+        if (time.time() - t) > time_limit:
+            print(f'WARNING: NMS time limit {time_limit}s exceeded')
+            break  # time limit exceeded
+
+    return output
+
+def compute_mAP(targets, predictions, iou_threshold=0.5):
+    """
+    Compute the mean Average Precision (mAP) for bounding box predictions.
+
+    Args:
+        targets (List[List[List[float]]]): List of ground truth bounding box coordinates for each sample.
+                                           Format: [[[x_min, y_min, x_max, y_max], ...], ...]
+        predictions (List[List[List[float]]]): List of predicted bounding box coordinates for each sample.
+                                                Format: [[[x_min, y_min, x_max, y_max, confidence], ...], ...]
+        iou_threshold (float): Intersection over Union (IOU) threshold for matching predictions to targets.
+
+    Returns:
+        float: mAP value.
+    """
+    num_samples = len(targets)
+    average_precisions = []
+
+    for sample_idx in range(num_samples):
+        target_boxes = targets[sample_idx]
+        prediction_boxes = predictions[sample_idx]
+
+        num_targets = len(target_boxes)
+        num_predictions = len(prediction_boxes)
+
+        if num_targets == 0 or num_predictions == 0:
+            continue
+
+        target_boxes = np.array(target_boxes)
+        prediction_boxes = np.array(prediction_boxes)
+
+        sorted_indices = np.argsort(-prediction_boxes[:, -1])
+        prediction_boxes = prediction_boxes[sorted_indices]
+
+        true_positives = np.zeros(num_predictions)
+        false_positives = np.zeros(num_predictions)
+        matched_targets = set()
+
+        for idx, prediction_box in enumerate(prediction_boxes):
+            predicted_box = prediction_box[:-1]
+            predicted_confidence = prediction_box[-1]
+
+            ious = box_iou(predicted_box, target_boxes)
+            max_iou = np.max(ious)
+            max_iou_index = np.argmax(ious)
+
+            if max_iou >= iou_threshold and max_iou_index not in matched_targets:
+                true_positives[idx] = 1
+                matched_targets.add(max_iou_index)
+            else:
+                false_positives[idx] = 1
+
+        cumsum_true_positives = np.cumsum(true_positives)
+        cumsum_false_positives = np.cumsum(false_positives)
+
+        precision = cumsum_true_positives / (cumsum_true_positives + cumsum_false_positives + 1e-16)
+        recall = cumsum_true_positives / num_targets
+
+        recall = np.concatenate(([0.0], recall, [1.0]))
+        precision = np.concatenate(([0.0], precision, [0.0]))
+
+        # Compute the interpolated precision
+        for i in range(len(precision) - 1, 0, -1):
+            precision[i - 1] = np.maximum(precision[i - 1], precision[i])
+
+        # Compute Average Precision (AP)
+        ap = np.sum((recall[1:] - recall[:-1]) * precision[:-1])
+
+        average_precisions.append(ap)
+
+    if len(average_precisions) == 0:
+        return 0.0
+
+    mAP = np.mean(average_precisions)
+    return mAP
